2e793508a430130f9ece414b02ae59c27d1a887f
abhiraj2 <abhiraj.official15@gmail.com>
2022-09-20 00:38:35 +0530

    'PATHPAR'

---
+++ codechef/PATHPAR.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long lol;
+typedef __int128_t lollol;
+typedef vector<int> vi;
+typedef vector<lol> vl;
+typedef pair<lol, lol> pl;
+#define MOD 1000000007
+#define itr(i,a,b) for (int i=a; i<=b; i++)
+
+
+int main(){
+	lol t;
+	cin >> t;
+	while(t--){
+	    lol n,k;
+	    cin >> n >> k;
+	    if(n%2 && !k) cout << "NO" << endl;
+	    else cout << "YES" << endl;
+	}
+}
